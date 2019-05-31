91a41253c53fb9b1a5cc56865fe3b654e86b1c32
DenisVdovenko <Vdovenko.Denis@gmail.com>
2019-05-31 10:42:51 +0300

    adding functionality to loginController

--- src/main/java/controler/LoginController.java
+++ src/main/java/controler/LoginController.java
@@ -1,4 +1,35 @@
-package servlets;
+package controler;
 
-public class LoginController {
+import service.LoginService;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/login")
+public class LoginController extends HttpServlet {
+
+    private LoginService loginService = new LoginService();
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.getRequestDispatcher("/login.jsp").forward(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String login = req.getParameter("login");
+        String password = req.getParameter("password");
+        boolean success = loginService.doLogin(login, password);
+        if (success){
+            req.getSession().setAttribute("login", login);
+            req.getSession().setAttribute("logged", true);
+            resp.sendRedirect("/user/todo");
+        }else {
+            req.getRequestDispatcher("/login.jsp").forward(req, resp);
+        }
+    }
 }
