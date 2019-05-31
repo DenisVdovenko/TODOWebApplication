59de227b13c7b866d69af4c352b8a6d9c377048b
DenisVdovenko <Vdovenko.Denis@gmail.com>
2019-05-31 10:33:57 +0300

    update read users from repository

--- src/main/java/repositories/UserRepository.java
+++ src/main/java/repositories/UserRepository.java
@@ -1,4 +1,37 @@
 package repositories;
 
+import model.TODO;
+import model.User;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class UserRepository {
+
+    private static String FILE_PATH = "/home/den/IdeaProjects/ToDoWebApplication/tododata/credentials.txt";
+
+    public List<User> getUsers() {
+        List<User> items = new ArrayList<>();
+        try (Scanner scanner = new Scanner(new FileInputStream(FILE_PATH))) {
+            long itemCount = scanner.nextLong();
+            for (long itemNumber = 0; itemNumber < itemCount; itemNumber++) {
+                extractUserFromScanner(items, scanner);
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println(e);
+        }
+        return items;
+    }
+
+    private void extractUserFromScanner(List<User> items, Scanner scanner) {
+        Long id = scanner.nextLong();
+        scanner.nextLine();
+        String login = scanner.nextLine();
+        String password = scanner.nextLine();
+        items.add(new User(id, login, password));
+    }
+
 }
